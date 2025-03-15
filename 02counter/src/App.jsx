d739fc081e43db58d7385913a602f67153a4b00c
alstongit <alstondsouza57@gmail.com>
2025-03-15 17:33:19 +0530

    learnt use state hook and benefit of ui updation in js vs react

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [counter, setCounter] = useState(15);
+  const addVal = ()=>{
+    setCounter(counter+1);    
+  }
+  const remVal = ()=>{
+    setCounter(counter-1);
+  }
+  return (
+    <>
+    <h1>Chai aur react</h1>
+    <h2>Counter value: {counter}</h2>
+    
+    <button onClick={addVal}>Add Value {counter}</button>
+    <br />
+    <button>Remove Value{counter}</button>
+    </>
+  )
+}
+
+export default App
